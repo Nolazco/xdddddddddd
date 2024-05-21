3b7c9352a3645bacce6ffa98f9bfb772c9aaf2c4
Nolazco <carlos_sceptile@hotmail.com>
2024-05-21 09:37:55 -0600

    feat: Button

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   const countMore = useCallback(() => setCount(count + 1), [count]);
 
   return (
-    <>
-      <h1>{count}</h1>
-      <button className="" onClick={countMore}>Mas</button>
-    </>
+    <main className="flex flex-col items-center p-12 gap-4">
+      <h1 className="text-4xl font-bold font-serif">{count}</h1>
+      <button className="px-4 py-3 rounded-xl bg-blue-300 m-1 font-bold" onClick={countMore}>Agregar uno</button>
+    </main>
   );
 }
 
