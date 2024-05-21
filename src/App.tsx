8c7aa5d85aa8e568504aa3466228220ea5b2eec9
Nolazco <carlos_sceptile@hotmail.com>
2024-05-21 09:28:36 -0600

    feat: Counting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import {useState, useCallback} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const countMore = useCallback(_ => setCount(count + 1), []);
 
   return (
     <>
+      <h1>{count}</h1>
+      <button onClick={countMore}></button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
