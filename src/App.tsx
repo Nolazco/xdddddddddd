d14f1de854080a89ff34ffcbb0cc0dc03ed31223
Nolazco <carlos_sceptile@hotmail.com>
2024-05-30 10:55:42 -0600

    feat: Countar menos

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import {useState, useCallback} from "react";
 function App() {
   const [count, setCount] = useState(0);
   const countMore = useCallback(() => setCount(count + 1), [count]);
+  const countLess = useCallback(() => setCount(count - 1), [count])
 
   return (
     <main className="flex flex-col items-center p-12 gap-4">
       <h1 className="text-4xl font-bold font-serif">{count}</h1>
       <button className="px-4 py-3 rounded-xl bg-blue-300 m-1 font-bold" onClick={countMore}>Agregar uno</button>
+      <button className="px-4 py-3 rounded-xl bg-blue-300 m-1 font-bold" onClick={countLess}>Quitar uno</button>
     </main>
   );
 }
